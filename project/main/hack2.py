8957415e65cc5586e2a95bf80dcd9083597d004c
KIMBIEW <jitkhon1979@gmail.com>
2025-03-02 16:24:18 +0700

    re name function

--- project/main/hack2.py
+++ project/main/hack2.py
@@ -10,28 +10,33 @@ def alternatingCharacters(s: str) -> int:
 
 
 class TestAlternatingCharacters(unittest.TestCase):
-    def test_cases(self):
-        self.assertEqual(alternatingCharacters("AABAAB"), 2)
+    def test_all_identical_characters(self):
         self.assertEqual(alternatingCharacters("AAAA"), 3)
         self.assertEqual(alternatingCharacters("BBBBB"), 4)
+        self.assertEqual(alternatingCharacters("A" * 1000), 999)
+        self.assertEqual(alternatingCharacters("B" * 1000), 999)
+
+    def test_alternating_characters(self):
         self.assertEqual(alternatingCharacters("ABABABAB"), 0)
         self.assertEqual(alternatingCharacters("BABABA"), 0)
-        self.assertEqual(alternatingCharacters("AAABBB"), 4)
-
-        self.assertEqual(alternatingCharacters("A"), 0)
-        self.assertEqual(alternatingCharacters("AB"), 0)
-        self.assertEqual(alternatingCharacters("AABB"), 2)
-        self.assertEqual(alternatingCharacters("ABBA"), 1)
-        self.assertEqual(alternatingCharacters("A" * 1000), 999)
         self.assertEqual(alternatingCharacters("AB" * 500), 0)
 
-        self.assertEqual(alternatingCharacters("B"), 0)
-        self.assertEqual(alternatingCharacters("BA"), 0)
-        self.assertEqual(alternatingCharacters("AA"), 1)
-        self.assertEqual(alternatingCharacters("BB"), 1)
+    def test_mixed_patterns(self):
+        self.assertEqual(alternatingCharacters("AABAAB"), 2)
+        self.assertEqual(alternatingCharacters("AAABBB"), 4)
+        self.assertEqual(alternatingCharacters("AABB"), 2)
+        self.assertEqual(alternatingCharacters("ABBA"), 1)
         self.assertEqual(alternatingCharacters("AAAABBBB"), 6)
         self.assertEqual(alternatingCharacters("BBBBBAAAAA"), 8)
         self.assertEqual(alternatingCharacters("ABABABAABB"), 2)
         self.assertEqual(alternatingCharacters("AABBABAB"), 2)
+
+    def test_edge_cases(self):
+        self.assertEqual(alternatingCharacters("A"), 0)
+        self.assertEqual(alternatingCharacters("B"), 0)
+        self.assertEqual(alternatingCharacters("AB"), 0)
+        self.assertEqual(alternatingCharacters("BA"), 0)
+        self.assertEqual(alternatingCharacters("AA"), 1)
+        self.assertEqual(alternatingCharacters("BB"), 1)
         self.assertEqual(alternatingCharacters("A" * 500 + "B" * 500), 998)
         self.assertEqual(alternatingCharacters("AB" * 250 + "A" * 500), 499)
