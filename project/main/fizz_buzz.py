3c5462ec7a79c72abe47a65ef4d7d5b11186845a
KIMBIEW <jitkhon1979@gmail.com>
2025-03-02 16:23:24 +0700

    re name function

--- project/main/fizz_buzz.py
+++ project/main/fizz_buzz.py
@@ -13,21 +13,29 @@ def fizzbuzz(x):
 
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_fizzbuzz(self):
+    def test_divisible_by_three(self):
         self.assertEqual(fizzbuzz(3), "Fizz")
+        self.assertEqual(fizzbuzz(9), "Fizz")
+        self.assertEqual(fizzbuzz(-3), "Fizz")
+
+    def test_divisible_by_five(self):
         self.assertEqual(fizzbuzz(5), "Buzz")
+        self.assertEqual(fizzbuzz(10), "Buzz")
+        self.assertEqual(fizzbuzz(-5), "Buzz")
+
+    def test_divisible_by_three_and_five(self):
         self.assertEqual(fizzbuzz(15), "FizzBuzz")
-        self.assertEqual(fizzbuzz(7), "7")
+        self.assertEqual(fizzbuzz(30), "FizzBuzz")
+        self.assertEqual(fizzbuzz(-15), "FizzBuzz")
+        self.assertEqual(fizzbuzz(0), "FizzBuzz")  # Special case
 
+    def test_not_divisible_by_three_or_five(self):
         self.assertEqual(fizzbuzz(1), "1")
-        self.assertEqual(fizzbuzz(0), "FizzBuzz")
+        self.assertEqual(fizzbuzz(7), "7")
+        self.assertEqual(fizzbuzz(1001), "1001")
+        self.assertEqual(fizzbuzz(-7), "-7")
 
+    def test_large_numbers(self):
         self.assertEqual(fizzbuzz(300), "FizzBuzz")
         self.assertEqual(fizzbuzz(999), "Fizz")
         self.assertEqual(fizzbuzz(1000), "Buzz")
-        self.assertEqual(fizzbuzz(1001), "1001")
-
-        self.assertEqual(fizzbuzz(-3), "Fizz")
-        self.assertEqual(fizzbuzz(-5), "Buzz")
-        self.assertEqual(fizzbuzz(-15), "FizzBuzz")
-        self.assertEqual(fizzbuzz(-7), "-7")
